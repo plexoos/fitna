117416110bd659e41d152e32367140447b1525a3
Dmitri Smirnov <d.s@plexoos.com>
2018-10-04 15:04:36 -0400

    Bump fitna version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "fitna",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Dmitri Smirnov",
     author_email = "dmixsmi@gmail.com",
     description = "Data fitting algorithms",
