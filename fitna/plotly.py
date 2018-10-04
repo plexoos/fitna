636577dc90c6112cc050e9144f653a4d68f308cd
Dmitri Smirnov <d.s@plexoos.com>
2018-10-04 14:37:46 -0400

    Replace helper routine making traces from a set of 2D arrays

--- fitna/plotly.py
+++ fitna/plotly.py
@@ -1,30 +1,21 @@
-import plotly.graph_objs as go
 import numpy as np
+import plotly
 import scipy
 
 
-def make_traces(estimates):
+def make_traces_from_arrays(data_points):
 
     traces = []
 
-    for estimate in estimates:
-        w, v = scipy.linalg.eigh(estimate.cov)
-        
-        errors = [dict( type = 'line', x0 = 0, y0 = 0, x1 = w[0]*v[0][0], y1 = w[0]*v[0][1] ),
-                  dict( type = 'line', x0 = 0, y0 = 0, x1 = w[1]*v[1][0], y1 = w[1]*v[1][1] )]
-        
-        mean_x = estimate.mean[0]
-        mean_y = estimate.mean[1]
-        
-        err_trace = go.Scatter(
-            x = [ w[0]*v[0][0] + mean_x, mean_x, w[1]*v[1][0] + mean_x ],
-            y = [ w[0]*v[0][1] + mean_y, mean_y, w[1]*v[1][1] + mean_y ],
-            mode = 'lines',
-            line = dict(width = 5),
-            hoverinfo = 'none'
+    for point_set in data_points:
+        trace = plotly.graph_objs.Scatter(
+            x=point_set[0],
+            y=point_set[1],
+            mode='markers',
+            hoverinfo='none'
         )
 
-        traces.append(err_trace)
+        traces.append(trace)
 
     return traces
 
